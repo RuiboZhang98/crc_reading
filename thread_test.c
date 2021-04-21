30c21c50e44649558e7553aab6661c6545b02d43
Ruibo Zhang <rzhang98@uw.edu>
2021-04-21 20:49:16 +0800

    test to see if threadlib works well

---
+++ thread_test.c
@@ -0,0 +1,20 @@
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <cmath>
+#include <map>
+#include <tuple>
+#include <vector>
+#include <set>
+#include <time.h>
+#include<thread>
+#include<atomic>
+#include<chrono>
+
+int main(){
+    unsigned int nCores = thread::hardware_concurrency();
+    int nThreads = nCores -2;
+    cout << "Now we have nCores= " <<  nCores << "avaliable cores"<< endl;
+    //cout << "nCores: " <<  nCores << endl;
+    return 0;
+}
